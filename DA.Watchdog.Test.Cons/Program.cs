7d9f7300a0aeb78e459fa75b65a15f85966992d9
André <andre.dunkel@t-online.de>
2023-06-23 18:15:44 +0200

    added the hangfire stuff; compiles - not working

--- DA.Watchdog.Test.Cons/Program.cs
+++ DA.Watchdog.Test.Cons/Program.cs
@@ -2,8 +2,9 @@
 using DA.Watchdog.Contracts;
 using DA.Watchdog.Model;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Linq;
+using Hangfire;
+using System;
 
 namespace DA.Watchdog.Test.Cons
 {
@@ -11,15 +12,28 @@ namespace DA.Watchdog.Test.Cons
 	{
 		static void Main(string[] args)
 		{
-			IWatcher watcher = ContractBinder.GetObject<IWatcher>();
-			using(WatchdogContext ctx = new WatchdogContext())
+			GlobalConfiguration.Configuration
+				.SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+				.UseSimpleAssemblyNameTypeSerializer()
+				.UseRecommendedSerializerSettings()
+				.UseColouredConsoleLogProvider()
+				.UseSqlServerStorage("Database=Hangfire;Integrated Security=true");
+
+			var hfClient = new BackgroundJobClient();
+			using (WatchdogContext ctx = new WatchdogContext())
 			{
 				var obs = ctx.Observable.Include(x => x.ObservableMeta).ToList();
 				obs.ForEach(x =>
 				{
+					IWatcher watcher = ContractBinder.GetObject<IWatcher>();
 					watcher.Init(x, ctx);
-					bool? result = watcher.CheckAsync(true).Result;
+					hfClient.Enqueue(() => watcher.CheckAsync(true));
 				});
+				// all jobs enqueued for one-time-execution, now start JobServer
+				using (var hfServer = new BackgroundJobServer())
+				{
+					Console.ReadLine();
+				}
 			}
 		}
 	}
